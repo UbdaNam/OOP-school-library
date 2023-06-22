2fa585db10b2e9564db3415e05e71310092a98fa
Abdurahim .M <Abdugreat2@gmail.com>
2023-06-22 11:26:59 +0300

    Add classes

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require "./person.rb"
+
+class Teacher
+  def initialize (name = "Unknown", age, parent_permission = true, specialization)
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
