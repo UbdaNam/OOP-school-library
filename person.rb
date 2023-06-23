c0ba21d6be2e4a00e873d2a152555d4879e11718
Abdurahim .M <Abdugreat2@gmail.com>
2023-06-23 06:43:47 +0300

    Update accessors position to top

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
@@ -6,8 +9,6 @@ class Person
     @parent_permission = parent_permission
   end
 
-  attr_accessor :name, :age
-  attr_reader :id
 
   def can_use_services?
     of_age? || @parent_permission
