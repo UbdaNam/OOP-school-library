966c7b51d2ba342e970976a13d245768e4de67cd
Abdurahim .M <Abdugreat2@gmail.com>
2023-06-24 14:11:14 +0300

    Add class book, classroom, and rentals with their associations

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ require './trimmer_decorator'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -12,6 +12,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
