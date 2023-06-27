86aa0d74aa162c28dcf67aa324f11193d491f42f
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-06-27 14:00:17 +0300

    add menu file

--- main.rb
+++ main.rb
@@ -1,30 +1,7 @@
 require_relative 'app'
+require_relative 'menu'
 
-def getOptions
-  {
-    1 => :list_all_books,
-    2 => :list_all_people,
-    3 => :create_person,
-    4 => :create_book,
-    5 => :create_rental,
-    6 => :list_rentals_by_id,
-    7 => :exit
-  }
-end
-
-def menu
-  puts 'Please choose an option:'
-  puts '1. List all books'
-  puts '2. List all people'
-  puts '3. Create a person'
-  puts '4. Create a book'
-  puts '5. Create a rental'
-  puts '6. List all rentals for a given person id'
-  puts '7. Exit'
-end
-
-def trigger_choice(option, app)
-  options = getOptions
+def trigger_choice(options, option, app)
   if options.key?(option)
     app.send(options[option])
   else
@@ -34,11 +11,13 @@ end
 
 def main
   app = App.new
+  menu = Menu.new
   puts 'Welcome to my School Library'
   loop do
-    menu
-    option = gets.chomp.to_i
-    trigger_choice(option, app)
+    menu.get_menu
+    user_choice = gets.chomp.to_i
+    options = menu.get_options
+    trigger_choice(options, user_choice, app)
   end
 end
 
