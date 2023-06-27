a2298414e4f1e11edf2d91731061a45b6fc108f8
Abdurahim .M <Abdugreat2@gmail.com>
2023-06-27 11:15:07 +0300

    Refactor code base

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require_relative 'app'
 
-def option_list(option, app)
-  options = {
+def getOptions
+  {
     1 => :list_all_books,
     2 => :list_all_people,
     3 => :create_person,
@@ -10,7 +10,21 @@ def option_list(option, app)
     6 => :list_rentals_by_id,
     7 => :exit
   }
+end
 
+def menu
+  puts 'Please choose an option:'
+  puts '1. List all books'
+  puts '2. List all people'
+  puts '3. Create a person'
+  puts '4. Create a book'
+  puts '5. Create a rental'
+  puts '6. List all rentals for a given person id'
+  puts '7. Exit'
+end
+
+def trigger_choice(option, app)
+  options = getOptions
   if options.key?(option)
     app.send(options[option])
   else
@@ -22,18 +36,9 @@ def main
   app = App.new
   puts 'Welcome to my School Library'
   loop do
-    puts 'Please choose an option:'
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List all rentals for a given person id'
-    puts '7. Exit'
-
+    menu
     option = gets.chomp.to_i
-
-    option_list(option, app)
+    trigger_choice(option, app)
   end
 end
 
