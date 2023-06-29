e5344520e137e3061317528cf96ff6e7e973a514
Abdurahim .M <Abdugreat2@gmail.com>
2023-06-29 23:22:18 +0300

    Fix linters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,17 +1,16 @@
-require_relative "../book.rb"
+require_relative '../book'
 
-describe Book do 
-  context "Tests for Book class" do
-    let(:book) { Book.new("Atomic habits", "Author 1") }
+describe Book do
+  context 'Tests for Book class' do
+    let(:book) { Book.new('Atomic habits', 'Author 1') }
 
-    it "Should create new book" do 
+    it 'Should create new book' do
       expect(book).to be_an_instance_of(Book)
     end
-    
-    it "Should create new book" do 
-      title = book.title
-      expect(title).to eq "Atomic habits"
-    end
 
+    it 'Should create new book' do
+      title = book.title
+      expect(title).to eq 'Atomic habits'
+    end
   end
 end
